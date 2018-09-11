31ffbbbf59aaa62a21e5f0a44eaf3c3bcaf5299f
Enrique Santos <me@eos.fyi>
2018-09-11 12:15:16 -0700

    fix indentation

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ function createTooltip() {
     var span= document.createElement("span");
     span.classList.add('toll-tooltip');
     span.classList.add('tooltip-' + (document.querySelectorAll('.toll-tooltip').length + 1));
-	  span.setAttribute("title", result.toFixed(2) + " 9/11s");
+    span.setAttribute("title", result.toFixed(2) + " 9/11s");
     span.appendChild(selectedText);
     selection.insertNode(span);
-	  console.log(result.toFixed(2) + " 9/11s");
+    console.log(result.toFixed(2) + " 9/11s");
 }
 
 var selection = window.getSelection().toString();
 var toll = parseFloat(selection.replace(/[^0-9.]/g, ''));
 
 if(isNaN(toll)) {
-	console.log("NaN");
+  console.log("NaN");
 } else {
-	var result = toll*(1/2977.0);
+  var result = toll*(1/2977.0);
   // 2977 casualties recorded (not including the hijackers, because fuck those guys)
   createTooltip();
   var tooltip = new Drooltip({
-		"element": ".tooltip-" + document.querySelectorAll('.toll-tooltip').length,
-		"position": "bottom"
-	});
+    "element": ".tooltip-" + document.querySelectorAll('.toll-tooltip').length,
+    "position": "bottom"
+  });
   tooltip.animateAllTooltips();
 }
